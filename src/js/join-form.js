736239d0ecb5e99c7c7853709db5b1fb76e242db
87233 <8723323@gmail.com>
2025-10-11 02:11:05 +0300

    f

--- src/js/join-form.js
+++ src/js/join-form.js
@@ -11,19 +11,23 @@ const ms = 3000;
 footerForm.addEventListener('submit', e => {
   e.preventDefault();
 
+  footerBtn.classList.remove('btn-error', 'btn-success');
+  remark.classList.remove('remark-error', 'remark-success');
+
   if (!footerInput.validity.valid) {
     if (footerInput.validity.valueMissing) {
       remark.textContent = '* Required field';
-      remark.style.color = 'var(--clr-dark)';
+      remark.classList.add('remark-default');
+
       iziToast.warning({
-    title: 'Caution',
-    message: 'Required field',
-    position: 'topRight',
-});
+        title: 'Caution',
+        message: 'Required field',
+        position: 'topRight',
+      });
     } else if (footerInput.validity.patternMismatch) {
       remark.textContent = 'Please enter a valid email';
-      remark.style.borderColor = 'var(--clr-error)';
-      footerBtn.style.borderColor = 'var(--clr-error)';
+      remark.classList.add('remark-error');
+      footerBtn.classList.add('btn-error');
       iziToast.error({
         title: 'Error',
         message: 'Please enter a valid email',
@@ -31,22 +35,28 @@ footerForm.addEventListener('submit', e => {
       });
     }
     setTimeout(() => {
-      (remark.textContent = ''),
-        (footerBtn.style.borderColor = 'var(--clr-dark)');
+      remark.textContent = '';
+      remark.classList.remove('remark-error', 'remark-success');
+      footerBtn.classList.remove('btn-error', 'btn-success');
     }, ms);
+    footerForm.reset();
   } else {
     remark.textContent = '✅ Email saved!';
-    remark.style.color = 'green';
-    footerBtn.style.backgroundColor = 'var(--clr-orange-dark)';
+    remark.classList.add('remark-success');
+    footerBtn.classList.add('btn-success');
+
     iziToast.success({
-    title: 'OK',
-    message: 'Email saved!',
-});
+      title: 'OK',
+      message: 'Email saved!',
+    });
 
     setTimeout(() => {
-      (remark.textContent = ''),
-        (footerBtn.style.backgroundColor = 'var(--clr-orange-light)');
+      remark.textContent = '';
+      remark.classList.remove('remark-success');
+      footerBtn.classList.remove('btn-success');
     }, ms);
+    emailBox.push(footerInput.value.trim());
+    console.log(emailBox);
     footerForm.reset();
   }
 });
