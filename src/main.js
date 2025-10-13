1014402ffca16aae341dda70cd67cecba8c1410c
SerhiiKutovyi <wagenrex@gmail.com>
2025-10-13 19:42:09 +0300

    fix styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import 'izitoast/dist/css/iziToast.min.css';
 import './js/burger-menu.js';
 import './js/slider-feedbacks';
 import './js/slider-hero.js';
 import './js/books.js';
 import './js/modal-book';
+import './js/handlers.js';
+import './js/helpers.js';
